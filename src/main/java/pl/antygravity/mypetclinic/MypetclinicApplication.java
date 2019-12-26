f5ba4f3db822113cd9bcdb940ea5918dbec8ecad
rawsk <rawski.andrzej@gmail.com>
2019-12-26 22:44:06 +0100

    Initial Project Add

---
+++ src/main/java/pl/antygravity/mypetclinic/MypetclinicApplication.java
@@ -0,0 +1,13 @@
+package pl.antygravity.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/antygravity/mypetclinic/MypetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.antygravity.mypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
