302cb14606caaf4dcd4cc6c1b0fab3d3b83438f9
rawsk <rawski.andrzej@gmail.com>
2020-01-13 11:51:45 +0100

    refactoring to multimodule maven project. closes #1.

---
+++ petClinicWeb/src/main/java/pl/antygravity/mypetclinic/MyPetClinicApplication.java
@@ -0,0 +1,11 @@
+package pl.antygravity.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetClinicApplication.class, args);
+    }
+}
