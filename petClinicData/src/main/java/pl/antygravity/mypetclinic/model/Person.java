772dc8d05175b288bc17b2f1216f454a19c99298
rawsk <rawski.andrzej@gmail.com>
2020-01-17 12:20:07 +0100

    Adding base entity. Closes #10.

--- petClinicData/src/main/java/pl/antygravity/mypetclinic/model/Person.java
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package pl.antygravity.mypetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- petClinicData/src/main/java/pl/antygravity/mypetclinic/model/PetType.java
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package pl.antygravity.mypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
