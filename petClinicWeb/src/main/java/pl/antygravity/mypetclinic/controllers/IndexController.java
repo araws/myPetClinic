fe0d166211e6f16958de3f162637ba2b619e7502
rawsk <rawski.andrzej@gmail.com>
2020-01-19 13:50:58 +0100

    Adding Index Page and controller. Closes #8.

---
+++ petClinicWeb/src/main/java/pl/antygravity/mypetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package pl.antygravity.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
