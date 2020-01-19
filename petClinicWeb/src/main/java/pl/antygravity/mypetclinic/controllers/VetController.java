30b0f9d25c565fb1a69c58a86fee3dc328989f04
rawsk <rawski.andrzej@gmail.com>
2020-01-19 14:41:23 +0100

    Adding Vets Page and controller. Closes #7.

---
+++ petClinicWeb/src/main/java/pl/antygravity/mypetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package pl.antygravity.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetController {
+
+    @GetMapping({"/vets", "/vets/index", "vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
