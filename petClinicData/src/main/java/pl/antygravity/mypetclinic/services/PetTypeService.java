c7c710288cc303586d14cf36291d25591ac7737a
rawsk <rawski.andrzej@gmail.com>
2020-02-03 23:05:50 +0100

    added PetTypeService. Closes #29.

---
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package pl.antygravity.mypetclinic.services;
+
+import pl.antygravity.mypetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package pl.antygravity.mypetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pl.antygravity.mypetclinic.model.PetType;
+import pl.antygravity.mypetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
