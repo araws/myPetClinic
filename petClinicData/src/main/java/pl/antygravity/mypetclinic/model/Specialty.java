f7f03bf957be89f234347189d840d23abbcda6df
rawsk <rawski.andrzej@gmail.com>
2020-02-21 14:16:11 +0100

    Updated Vet with JPA entity mappings. Closes #37.

--- petClinicData/src/main/java/pl/antygravity/mypetclinic/model/Specialty.java
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package pl.antygravity.mypetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
