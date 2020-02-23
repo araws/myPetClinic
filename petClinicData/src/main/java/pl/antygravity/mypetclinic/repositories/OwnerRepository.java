bd345d3d1c63579948b75536007509670f49ca50
rawsk <rawski.andrzej@gmail.com>
2020-02-23 21:45:28 +0100

    Added Spring Data JPA Service for Owners. Closes #40.

--- petClinicData/src/main/java/pl/antygravity/mypetclinic/repositories/OwnerRepository.java
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import pl.antygravity.mypetclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
