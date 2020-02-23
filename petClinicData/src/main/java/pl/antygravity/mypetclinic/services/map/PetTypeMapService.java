9656d74012b679e056994acf04aa973670a99576
rawsk <rawski.andrzej@gmail.com>
2020-02-23 22:08:22 +0100

    Refactoring

--- petClinicData/src/main/java/pl/antygravity/mypetclinic/services/map/PetTypeMapService.java
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/services/map/PetTypeMapService.java
@@ -7,7 +7,7 @@ import pl.antygravity.mypetclinic.services.PetTypeService;
 import java.util.Set;
 
 @Service
-public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
 
     @Override
     public Set<PetType> findAll() {
