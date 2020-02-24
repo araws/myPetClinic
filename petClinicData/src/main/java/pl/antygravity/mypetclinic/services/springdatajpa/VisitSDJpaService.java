08197c63e76c02dd5e4312f55cdb38f831687f19
rawsk <rawski.andrzej@gmail.com>
2020-02-24 23:12:25 +0100

    Added Spring Data JPA Service for Visits. Closes #47.

---
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package pl.antygravity.mypetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import pl.antygravity.mypetclinic.model.Visit;
+import pl.antygravity.mypetclinic.repositories.VisitRepository;
+import pl.antygravity.mypetclinic.services.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
