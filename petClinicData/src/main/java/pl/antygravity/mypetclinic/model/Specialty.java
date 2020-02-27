8c745d3fed7e60cc45fb676a2bb291bf411f0a6a
rawsk <rawski.andrzej@gmail.com>
2020-02-27 13:42:21 +0100

    Added Project Lombok to Project. Closes #51.

--- petClinicData/src/main/java/pl/antygravity/mypetclinic/model/Specialty.java
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/model/Specialty.java
@@ -1,21 +1,20 @@
 package pl.antygravity.mypetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Specialty extends BaseEntity {
 
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- petClinicData/src/main/java/pl/antygravity/mypetclinic/model/Vet.java
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package pl.antygravity.mypetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -13,12 +20,4 @@ public class Vet extends Person {
             joinColumns = @JoinColumn(name = "vet_id"),
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Specialty> specialties = new HashSet<>();
-
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
