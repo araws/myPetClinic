462faddd3e27b181632f1a997f20d94f8835f447
rawsk <rawski.andrzej@gmail.com>
2020-03-07 00:39:04 +0100

    Added tests to OwnerMapService. Closes #49.

--- petClinicData/src/main/java/pl/antygravity/mypetclinic/services/map/AbstractMapService.java
+++ petClinicData/src/main/java/pl/antygravity/mypetclinic/services/map/AbstractMapService.java
@@ -8,7 +8,7 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
 
     protected Map<Long, T> map = new HashMap<>();
 
-    Set<T> findAll() {
+    Set<T> findAll(){
         return new HashSet<>(map.values());
     }
 
@@ -16,29 +16,30 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
         return map.get(id);
     }
 
-    T save(T object) {
+    T save(T object){
 
-        if (object != null) {
-            if (object.getId() == null) {
+        if(object != null) {
+            if(object.getId() == null){
                 object.setId(getNextId());
-            } else {
-                throw new RuntimeException("Object cannot be null");
             }
 
-            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
         }
+        map.put(object.getId(), object);
+
         return object;
     }
 
-    void deleteById(ID id) {
+    void deleteById(ID id){
         map.remove(id);
     }
 
-    void delete(T object) {
+    void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
-    private Long getNextId() {
+    private Long getNextId(){
 
         Long nextId = null;
 
