4378b3681b94f8b896c8c2a69351e56b03dfe3cc
Andrzej Rawski <rawski.andrzej@gmail.com>
2021-02-09 14:13:22 +0100

    Create Spring MockMVC Tests for Visit Controller. Closes #66 & #67

--- petClinicWeb/src/main/java/pl/antygravity/mypetclinic/controllers/VisitController.java
+++ petClinicWeb/src/main/java/pl/antygravity/mypetclinic/controllers/VisitController.java
@@ -1,7 +1,6 @@
 package pl.antygravity.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +10,12 @@ import pl.antygravity.mypetclinic.services.PetService;
 import pl.antygravity.mypetclinic.services.VisitService;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+import java.util.Map;
 
 @Controller
+@RequestMapping("/owners/*/pets/{petId}/visits")
 public class VisitController {
 
     private final VisitService visitService;
@@ -24,8 +27,14 @@ public class VisitController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
+    public void dataBinder(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     /**
@@ -40,9 +49,9 @@ public class VisitController {
      */
 
     @ModelAttribute("visit")
-    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Model model) {
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Map<String, Object> model) {
         Pet pet = petService.findById(petId);
-        model.addAttribute("pet", pet);
+        model.put("pet", pet);
         Visit visit = new Visit();
         pet.getVisits().add(visit);
         visit.setPet(pet);
@@ -50,13 +59,13 @@ public class VisitController {
     }
 
     //Spring MVC calls method loadPetWithVisit(...) before initNewVisitForm is called
-    @GetMapping("/owners/*/pets/{petId}/visits/new")
-    public String initNewVisitForm(@PathVariable Long petId, Model model) {
+    @GetMapping("/new")
+    public String initNewVisitForm(@PathVariable Long petId, Map<String, Object> model) {
         return "pets/createOrUpdateVisitForm";
     }
 
     //Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
-    @PostMapping("/owners/{ownerId}}/pets/{petId}/visits/new")
+    @PostMapping("/new")
     public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
         if (result.hasErrors()) {
             return "pets/createOrUpdateVisitForm";
